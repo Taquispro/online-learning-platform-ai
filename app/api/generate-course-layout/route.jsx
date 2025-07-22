dbb8f8e5d70da522fc1134400acf98585ba16195
Taquispro <121485751+Taquispro@users.noreply.github.com>
2025-07-22 20:22:09 +0530

    bug fix

--- app/api/generate-course-layout/route.jsx
+++ app/api/generate-course-layout/route.jsx
@@ -6,7 +6,7 @@ import axios from "axios";
 import { eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
 
-const PROMPT = `Genrate Learning Course depends on following details. In which Make sure to add Course Name, Description,Course Banner Image Prompt (Create a modern, flat-style 2D digital illustration representing user Topic. Include UI/UX elements such as mockup screens, text blocks, icons, buttons, and creative workspace tools. Add symbolic elements related to user Course, like sticky notes, design components, and visual aids. Use a vibrant color palette (blues, purples, oranges) with a clean, professional look. The illustration should feel creative, tech-savvy, and educational, ideal for visualizing concepts in user Course) for Course Banner in 3d format Chapter Name, , Topic under each chapters , Duration for each chapters etc, in JSON format only
+const PROMPT = `Genrate Learning Course depends on following details: In which Make sure to add Course Name, Description,Course Banner Image Prompt (Create a modern, flat-style 2D digital illustration representing user Topic. Include UI/UX elements such as mockup screens, text blocks, icons, buttons, and creative workspace tools. Add symbolic elements related to user Course, like sticky notes, design components, and visual aids. Use a vibrant color palette (blues, purples, oranges) with a clean, professional look. The illustration should feel creative, tech-savvy, and educational, ideal for visualizing concepts in user Course) for Course Banner in 3d format Chapter Name, , Topic under each chapters , Duration for each chapters etc, in JSON format only
 
 Schema:
 
